0319b8be2c4f12590ec2fb0e735e5a8044e60ee0
Nont Banditwong <nontster@gmail.com>
2016-12-19 17:28:27 +0700

    Add test for RESTful web services

--- src/test/java/th/co/ais/enterprisecloud/automation/LazybeeApplicationTests.java
+++ src/test/java/th/co/ais/enterprisecloud/automation/LazybeeApplicationTests.java
@@ -2,15 +2,22 @@ package th.co.ais.enterprisecloud.automation;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
 public class LazybeeApplicationTests {
 
+	@Autowired
+    private TestRestTemplate restTemplate;
+	
 	@Test
-	public void contextLoads() {
+	public void testProvisioningService() {
+		//this.restTemplate.postForEntity(url, request, responseType)
 	}
 
 }

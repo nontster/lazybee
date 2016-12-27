50ca8fd28525d82a42dc639bfbfb2184751c8518
nontster <nontster@172.17.4.93>
2016-12-27 16:42:02 +0700

    Add connect method to CloudService interface

--- src/main/java/th/co/ais/enterprisecloud/service/CloudService.java
+++ src/main/java/th/co/ais/enterprisecloud/service/CloudService.java
@@ -2,6 +2,9 @@ package th.co.ais.enterprisecloud.service;
 
 import java.util.concurrent.Future;
 
+import com.vmware.vcloud.sdk.VCloudException;
+
 public interface CloudService {
-	public Future<Boolean> provisioning(th.co.ais.enterprisecloud.model.request.OrganizationType org) throws InterruptedException;
+	public void connect() throws VCloudException;
+	public Future<Boolean> provisioning(th.co.ais.enterprisecloud.model.request.OrganizationType org) throws InterruptedException, VCloudException;
 }

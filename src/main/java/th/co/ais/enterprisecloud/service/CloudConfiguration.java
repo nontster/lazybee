f74228fadd1d8e630b570ad4a92bf4f5bee93842
nontster <nontster@172.17.4.93>
2016-12-27 16:31:28 +0700

    Change XML Schema name from bot_request.xsd to request.xsd and from bot_response.xsd to response.xsd Change some classes name

--- src/main/java/th/co/ais/enterprisecloud/service/CloudConfiguration.java
+++
@@ -1,7 +0,0 @@
-package th.co.ais.enterprisecloud.service;
-
-import com.vmware.vcloud.sdk.VCloudException;
-
-public interface CloudConfiguration {
-	public void connect() throws VCloudException;
-}

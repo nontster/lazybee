ce32725616dface6cc869469c11867f8daa7dfcb
nontster <nontster@192.168.1.101>
2017-01-09 20:53:51 +0700

    Add transfer() prepareResponse() method Change response object of provisioning() method from Future<Boolean> to Future<OrganizationType>

--- src/main/java/th/co/ais/enterprisecloud/service/CloudService.java
+++ src/main/java/th/co/ais/enterprisecloud/service/CloudService.java
@@ -1,10 +1,20 @@
 package th.co.ais.enterprisecloud.service;
 
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeoutException;
 
 import com.vmware.vcloud.sdk.VCloudException;
+import com.vmware.vcloud.sdk.Vapp;
+import com.vmware.vcloud.sdk.admin.EdgeGateway;
 
 public interface CloudService {
 	public void connect() throws VCloudException;
-	public Future<Boolean> provisioning(th.co.ais.enterprisecloud.model.request.OrganizationType org) throws InterruptedException, VCloudException;
+
+	public th.co.ais.enterprisecloud.domain.OrganizationType transfer(
+			th.co.ais.enterprisecloud.domain.request.OrganizationType org);
+
+	public th.co.ais.enterprisecloud.domain.response.OrganizationType prepareResponse(
+			th.co.ais.enterprisecloud.domain.OrganizationType org, EdgeGateway edgeGateway, Vapp vapp, String orgVdcName) throws VCloudException;
+	
+	public Future<th.co.ais.enterprisecloud.domain.response.OrganizationType> provisioning(th.co.ais.enterprisecloud.domain.OrganizationType in) throws VCloudException, TimeoutException;
 }

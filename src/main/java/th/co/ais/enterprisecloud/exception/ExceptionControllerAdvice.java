ebc283be9686476ae0eb842fa05926a3ee4c7ffd
nontster <nontster@10.104.166.32>
2017-01-09 11:08:37 +0700

    Add Spring global exception handler

---
+++ src/main/java/th/co/ais/enterprisecloud/exception/ExceptionControllerAdvice.java
@@ -0,0 +1,46 @@
+package th.co.ais.enterprisecloud.exception;
+
+import java.sql.Timestamp;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionControllerAdvice extends ResponseEntityExceptionHandler{
+
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler({ ExternalNetworkNotFoundException.class, InsufficientIPAddressesException.class,
+			MissingVmTemplateException.class, UserRoleNotFoundException.class, VdcNetworkNotAvailableException.class })
+	public @ResponseBody ExceptionResponse handleNotFoundException(HttpServletRequest request, RuntimeException ex) {
+		ExceptionResponse response = new ExceptionResponse();
+		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+		
+		response.setTimestamp(timestamp.getTime());
+		response.setStatus(HttpStatus.NOT_FOUND.value());
+		response.setPath(request.getServletPath()); 
+		response.setMessage(ex.getMessage());
+
+		return response;
+	}
+
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler({MissingInputParameterException.class, InvalidParameterException.class, InvalidTemplateException.class})
+	public @ResponseBody ExceptionResponse handleBadRequestException(HttpServletRequest request, RuntimeException ex) {
+		ExceptionResponse response = new ExceptionResponse();
+		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+		
+		response.setTimestamp(timestamp.getTime());
+		response.setStatus(HttpStatus.NOT_FOUND.value());
+		response.setPath(request.getServletPath()); 
+		response.setMessage(ex.getMessage());
+
+		return response;
+	}
+	
+}
---
+++ src/main/java/th/co/ais/enterprisecloud/exception/ExceptionResponse.java
@@ -0,0 +1,59 @@
+package th.co.ais.enterprisecloud.exception;
+
+public class ExceptionResponse {
+	
+	private long timestamp;
+	private int status;
+	private String path;
+	private String message;
+	
+	/**
+	 * @return the timestamp
+	 */
+	public long getTimestamp() {
+		return timestamp;
+	}
+	/**
+	 * @param timestamp the timestamp to set
+	 */
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+	/**
+	 * @return the status
+	 */
+	public int getStatus() {
+		return status;
+	}
+	/**
+	 * @param status the status to set
+	 */
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	/**
+	 * @return the path
+	 */
+	public String getPath() {
+		return path;
+	}
+	/**
+	 * @param path the path to set
+	 */
+	public void setPath(String path) {
+		this.path = path;
+	}
+	/**
+	 * @return the message
+	 */
+	public String getMessage() {
+		return message;
+	}
+	/**
+	 * @param message the message to set
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}	
+}

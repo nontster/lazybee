a7c77e2b9b78b401678bef15731ad22e63ba9879
nontster <nontster@172.17.4.211>
2017-01-10 16:03:41 +0700

    Add more Unit Test

--- src/test/java/th/co/ais/enterprisecloud/automation/LazybeeApplicationTests.java
+++ src/test/java/th/co/ais/enterprisecloud/automation/LazybeeApplicationTests.java
@@ -37,8 +37,7 @@ public class LazybeeApplicationTests {
     private TestRestTemplate restTemplate;
 	
 	@Test
-	public void testProvisioningService() throws JAXBException {
-		
+	public void provisioningSuccessTest() throws JAXBException {		
 		String body = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><organization name=\"Virgin Media Limited\" orderId=\"09101010102939\" caNumber=\"90111045678\" orderType=\"trial\" shortName=\"VMED\" xmlns=\"http://enterprisecloud.ais.co.th/bot\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><users><user email=\"john.doe@virgin.com\" fullName=\"John Doe\" phone=\"0613952200\" /></users><vms><vm coresPerSocket=\"2\" memorySize=\"4\" noOfCpus=\"2\" nonMobileNumber=\"1234567890\" osImageName=\"CENTOS7\" /></vms></organization>";
 		
 		HttpHeaders headers = new HttpHeaders();
@@ -61,4 +60,263 @@ public class LazybeeApplicationTests {
 		
 	}
 
+	@Test
+	public void missingOrderIdTest() throws JAXBException{
+		String body = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><organization name=\"Virgin Media Limited\" caNumber=\"90111045678\" orderType=\"trial\" shortName=\"VMED\" xmlns=\"http://enterprisecloud.ais.co.th/bot\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><users><user email=\"john.doe@virgin.com\" fullName=\"John Doe\" phone=\"0613952200\" /></users><vms><vm coresPerSocket=\"2\" memorySize=\"4\" noOfCpus=\"2\" nonMobileNumber=\"1234567890\" osImageName=\"CENTOS7\" /></vms></organization>";
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_XML);
+		headers.add("Accept-Charset", "UTF-8");
+		headers.add("Accept", "text/xml, application/json");
+		
+		HttpEntity<String> request = new HttpEntity<>(body, headers);
+		ResponseEntity<String> response = this.restTemplate.postForEntity("http://localhost:" + port + "/orgs", request, String.class);
+					
+		assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.NOT_FOUND);
+
+	}
+	
+	@Test
+	public void missingCaNumberTest(){
+		
+		String body = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><organization name=\"Virgin Media Limited\" orderId=\"09101010102939\" orderType=\"trial\" shortName=\"VMED\" xmlns=\"http://enterprisecloud.ais.co.th/bot\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><users><user email=\"john.doe@virgin.com\" fullName=\"John Doe\" phone=\"0613952200\" /></users><vms><vm coresPerSocket=\"2\" memorySize=\"4\" noOfCpus=\"2\" nonMobileNumber=\"1234567890\" osImageName=\"CENTOS7\" /></vms></organization>";
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_XML);
+		headers.add("Accept-Charset", "UTF-8");
+		headers.add("Accept", "text/xml, application/json");
+		
+		HttpEntity<String> request = new HttpEntity<>(body, headers);
+		ResponseEntity<String> response = this.restTemplate.postForEntity("http://localhost:" + port + "/orgs", request, String.class);
+					
+		assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.NOT_FOUND);
+	}
+	
+	@Test
+	public void missingCustomerNameTest(){
+		String body = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><organization orderId=\"09101010102939\" caNumber=\"90111045678\" orderType=\"trial\" shortName=\"VMED\" xmlns=\"http://enterprisecloud.ais.co.th/bot\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><users><user email=\"john.doe@virgin.com\" fullName=\"John Doe\" phone=\"0613952200\" /></users><vms><vm coresPerSocket=\"2\" memorySize=\"4\" noOfCpus=\"2\" nonMobileNumber=\"1234567890\" osImageName=\"CENTOS7\" /></vms></organization>";
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_XML);
+		headers.add("Accept-Charset", "UTF-8");
+		headers.add("Accept", "text/xml, application/json");
+		
+		HttpEntity<String> request = new HttpEntity<>(body, headers);
+		ResponseEntity<String> response = this.restTemplate.postForEntity("http://localhost:" + port + "/orgs", request, String.class);
+					
+		assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.NOT_FOUND);
+	}
+	
+	@Test
+	public void missingOrderTypeTest(){
+		String body = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><organization name=\"Virgin Media Limited\" orderId=\"09101010102939\" caNumber=\"90111045678\" shortName=\"VMED\" xmlns=\"http://enterprisecloud.ais.co.th/bot\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><users><user email=\"john.doe@virgin.com\" fullName=\"John Doe\" phone=\"0613952200\" /></users><vms><vm coresPerSocket=\"2\" memorySize=\"4\" noOfCpus=\"2\" nonMobileNumber=\"1234567890\" osImageName=\"CENTOS7\" /></vms></organization>";
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_XML);
+		headers.add("Accept-Charset", "UTF-8");
+		headers.add("Accept", "text/xml, application/json");
+		
+		HttpEntity<String> request = new HttpEntity<>(body, headers);
+		ResponseEntity<String> response = this.restTemplate.postForEntity("http://localhost:" + port + "/orgs", request, String.class);
+					
+		assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.NOT_FOUND);
+	}
+	
+	@Test
+	public void wrongOrderTypeTest(){
+		String body = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><organization name=\"Virgin Media Limited\" orderId=\"09101010102939\" caNumber=\"90111045678\" orderType=\"tryal\" shortName=\"VMED\" xmlns=\"http://enterprisecloud.ais.co.th/bot\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><users><user email=\"john.doe@virgin.com\" fullName=\"John Doe\" phone=\"0613952200\" /></users><vms><vm coresPerSocket=\"2\" memorySize=\"4\" noOfCpus=\"2\" nonMobileNumber=\"1234567890\" osImageName=\"CENTOS7\" /></vms></organization>";
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_XML);
+		headers.add("Accept-Charset", "UTF-8");
+		headers.add("Accept", "text/xml, application/json");
+		
+		HttpEntity<String> request = new HttpEntity<>(body, headers);
+		ResponseEntity<String> response = this.restTemplate.postForEntity("http://localhost:" + port + "/orgs", request, String.class);
+					
+		assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.NOT_FOUND);
+	}
+	
+	@Test
+	public void missingShortNameTest(){
+		String body = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><organization name=\"Virgin Media Limited\" orderId=\"09101010102939\" caNumber=\"90111045678\" orderType=\"trial\" xmlns=\"http://enterprisecloud.ais.co.th/bot\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><users><user email=\"john.doe@virgin.com\" fullName=\"John Doe\" phone=\"0613952200\" /></users><vms><vm coresPerSocket=\"2\" memorySize=\"4\" noOfCpus=\"2\" nonMobileNumber=\"1234567890\" osImageName=\"CENTOS7\" /></vms></organization>";
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_XML);
+		headers.add("Accept-Charset", "UTF-8");
+		headers.add("Accept", "text/xml, application/json");
+		
+		HttpEntity<String> request = new HttpEntity<>(body, headers);
+		ResponseEntity<String> response = this.restTemplate.postForEntity("http://localhost:" + port + "/orgs", request, String.class);
+					
+		assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.NOT_FOUND);
+	}
+	
+	@Test
+	public void missingUsersTest(){
+		String body = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><organization name=\"Virgin Media Limited\" orderId=\"09101010102939\" caNumber=\"90111045678\" orderType=\"trial\" shortName=\"VMED\" xmlns=\"http://enterprisecloud.ais.co.th/bot\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><vms><vm coresPerSocket=\"2\" memorySize=\"4\" noOfCpus=\"2\" nonMobileNumber=\"1234567890\" osImageName=\"CENTOS7\" /></vms></organization>";
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_XML);
+		headers.add("Accept-Charset", "UTF-8");
+		headers.add("Accept", "text/xml, application/json");
+		
+		HttpEntity<String> request = new HttpEntity<>(body, headers);
+		ResponseEntity<String> response = this.restTemplate.postForEntity("http://localhost:" + port + "/orgs", request, String.class);
+					
+		assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.NOT_FOUND);
+	}
+	
+	@Test
+	public void missingUserTest(){
+		String body = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><organization name=\"Virgin Media Limited\" orderId=\"09101010102939\" caNumber=\"90111045678\" orderType=\"trial\" shortName=\"VMED\" xmlns=\"http://enterprisecloud.ais.co.th/bot\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><users></users><vms><vm coresPerSocket=\"2\" memorySize=\"4\" noOfCpus=\"2\" nonMobileNumber=\"1234567890\" osImageName=\"CENTOS7\" /></vms></organization>";
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_XML);
+		headers.add("Accept-Charset", "UTF-8");
+		headers.add("Accept", "text/xml, application/json");
+		
+		HttpEntity<String> request = new HttpEntity<>(body, headers);
+		ResponseEntity<String> response = this.restTemplate.postForEntity("http://localhost:" + port + "/orgs", request, String.class);
+					
+		assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.NOT_FOUND);
+	}
+	
+	@Test
+	public void missingUserEmailTest(){
+		String body = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><organization name=\"Virgin Media Limited\" orderId=\"09101010102939\" caNumber=\"90111045678\" orderType=\"trial\" shortName=\"VMED\" xmlns=\"http://enterprisecloud.ais.co.th/bot\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><users><user fullName=\"John Doe\" phone=\"0613952200\" /></users><vms><vm coresPerSocket=\"2\" memorySize=\"4\" noOfCpus=\"2\" nonMobileNumber=\"1234567890\" osImageName=\"CENTOS7\" /></vms></organization>";
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_XML);
+		headers.add("Accept-Charset", "UTF-8");
+		headers.add("Accept", "text/xml, application/json");
+		
+		HttpEntity<String> request = new HttpEntity<>(body, headers);
+		ResponseEntity<String> response = this.restTemplate.postForEntity("http://localhost:" + port + "/orgs", request, String.class);
+					
+		assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.NOT_FOUND);
+	}
+	
+	@Test
+	public void missingUserFullNameTest(){
+		String body = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><organization name=\"Virgin Media Limited\" orderId=\"09101010102939\" caNumber=\"90111045678\" orderType=\"trial\" shortName=\"VMED\" xmlns=\"http://enterprisecloud.ais.co.th/bot\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><users><user email=\"john.doe@virgin.com\" phone=\"0613952200\" /></users><vms><vm coresPerSocket=\"2\" memorySize=\"4\" noOfCpus=\"2\" nonMobileNumber=\"1234567890\" osImageName=\"CENTOS7\" /></vms></organization>";
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_XML);
+		headers.add("Accept-Charset", "UTF-8");
+		headers.add("Accept", "text/xml, application/json");
+		
+		HttpEntity<String> request = new HttpEntity<>(body, headers);
+		ResponseEntity<String> response = this.restTemplate.postForEntity("http://localhost:" + port + "/orgs", request, String.class);
+					
+		assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.NOT_FOUND);
+	}
+	
+	@Test
+	public void missingVmsTest(){
+		String body = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><organization name=\"Virgin Media Limited\" orderId=\"09101010102939\" caNumber=\"90111045678\" orderType=\"trial\" shortName=\"VMED\" xmlns=\"http://enterprisecloud.ais.co.th/bot\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><users><user email=\"john.doe@virgin.com\" fullName=\"John Doe\" phone=\"0613952200\" /></users></organization>";
+		
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_XML);
+		headers.add("Accept-Charset", "UTF-8");
+		headers.add("Accept", "text/xml, application/json");
+		
+		HttpEntity<String> request = new HttpEntity<>(body, headers);
+		ResponseEntity<String> response = this.restTemplate.postForEntity("http://localhost:" + port + "/orgs", request, String.class);
+					
+		assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.NOT_FOUND);
+	}
+	
+	@Test
+	public void missingVmTest(){
+		String body = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><organization name=\"Virgin Media Limited\" orderId=\"09101010102939\" caNumber=\"90111045678\" orderType=\"trial\" shortName=\"VMED\" xmlns=\"http://enterprisecloud.ais.co.th/bot\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><users><user email=\"john.doe@virgin.com\" fullName=\"John Doe\" phone=\"0613952200\" /></users><vms></vms></organization>";
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_XML);
+		headers.add("Accept-Charset", "UTF-8");
+		headers.add("Accept", "text/xml, application/json");
+		
+		HttpEntity<String> request = new HttpEntity<>(body, headers);
+		ResponseEntity<String> response = this.restTemplate.postForEntity("http://localhost:" + port + "/orgs", request, String.class);
+					
+		assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.NOT_FOUND);
+	}	
+
+	@Test
+	public void missingVmCoresPerSocketTest(){
+		String body = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><organization name=\"Virgin Media Limited\" orderId=\"09101010102939\" caNumber=\"90111045678\" orderType=\"trial\" shortName=\"VMED\" xmlns=\"http://enterprisecloud.ais.co.th/bot\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><users><user email=\"john.doe@virgin.com\" fullName=\"John Doe\" phone=\"0613952200\" /></users><vms><vm memorySize=\"4\" noOfCpus=\"2\" nonMobileNumber=\"1234567890\" osImageName=\"CENTOS7\" /></vms></organization>";
+		
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_XML);
+		headers.add("Accept-Charset", "UTF-8");
+		headers.add("Accept", "text/xml, application/json");
+		
+		HttpEntity<String> request = new HttpEntity<>(body, headers);
+		ResponseEntity<String> response = this.restTemplate.postForEntity("http://localhost:" + port + "/orgs", request, String.class);
+					
+		assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.NOT_FOUND);
+	}
+	
+	@Test
+	public void missingVmNoOfCpusTest(){
+		String body = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><organization name=\"Virgin Media Limited\" orderId=\"09101010102939\" caNumber=\"90111045678\" orderType=\"trial\" shortName=\"VMED\" xmlns=\"http://enterprisecloud.ais.co.th/bot\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><users><user email=\"john.doe@virgin.com\" fullName=\"John Doe\" phone=\"0613952200\" /></users><vms><vm coresPerSocket=\"2\" memorySize=\"4\" nonMobileNumber=\"1234567890\" osImageName=\"CENTOS7\" /></vms></organization>";
+				
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_XML);
+		headers.add("Accept-Charset", "UTF-8");
+		headers.add("Accept", "text/xml, application/json");
+		
+		HttpEntity<String> request = new HttpEntity<>(body, headers);
+		ResponseEntity<String> response = this.restTemplate.postForEntity("http://localhost:" + port + "/orgs", request, String.class);
+					
+		assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.NOT_FOUND);
+	}	
+	
+	@Test
+	public void missingVmMemoryTest(){
+		String body = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><organization name=\"Virgin Media Limited\" orderId=\"09101010102939\" caNumber=\"90111045678\" orderType=\"trial\" shortName=\"VMED\" xmlns=\"http://enterprisecloud.ais.co.th/bot\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><users><user email=\"john.doe@virgin.com\" fullName=\"John Doe\" phone=\"0613952200\" /></users><vms><vm coresPerSocket=\"2\" noOfCpus=\"2\" nonMobileNumber=\"1234567890\" osImageName=\"CENTOS7\" /></vms></organization>";
+				
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_XML);
+		headers.add("Accept-Charset", "UTF-8");
+		headers.add("Accept", "text/xml, application/json");
+		
+		HttpEntity<String> request = new HttpEntity<>(body, headers);
+		ResponseEntity<String> response = this.restTemplate.postForEntity("http://localhost:" + port + "/orgs", request, String.class);
+					
+		assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.NOT_FOUND);
+	}
+	
+	@Test
+	public void missingOsImageNameTest(){
+		String body = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><organization name=\"Virgin Media Limited\" orderId=\"09101010102939\" caNumber=\"90111045678\" orderType=\"trial\" shortName=\"VMED\" xmlns=\"http://enterprisecloud.ais.co.th/bot\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><users><user email=\"john.doe@virgin.com\" fullName=\"John Doe\" phone=\"0613952200\" /></users><vms><vm coresPerSocket=\"2\" memorySize=\"4\" noOfCpus=\"2\" nonMobileNumber=\"1234567890\"/></vms></organization>";
+				
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_XML);
+		headers.add("Accept-Charset", "UTF-8");
+		headers.add("Accept", "text/xml, application/json");
+		
+		HttpEntity<String> request = new HttpEntity<>(body, headers);
+		ResponseEntity<String> response = this.restTemplate.postForEntity("http://localhost:" + port + "/orgs", request, String.class);
+					
+		assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.NOT_FOUND);
+	}	
+	
+	@Test
+	public void missingNonMobileNumberTest(){
+		String body = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><organization name=\"Virgin Media Limited\" orderId=\"09101010102939\" caNumber=\"90111045678\" orderType=\"trial\" shortName=\"VMED\" xmlns=\"http://enterprisecloud.ais.co.th/bot\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><users><user email=\"john.doe@virgin.com\" fullName=\"John Doe\" phone=\"0613952200\" /></users><vms><vm coresPerSocket=\"2\" memorySize=\"4\" noOfCpus=\"2\" osImageName=\"CENTOS7\" /></vms></organization>";
+				
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_XML);
+		headers.add("Accept-Charset", "UTF-8");
+		headers.add("Accept", "text/xml, application/json");
+		
+		HttpEntity<String> request = new HttpEntity<>(body, headers);
+		ResponseEntity<String> response = this.restTemplate.postForEntity("http://localhost:" + port + "/orgs", request, String.class);
+					
+		assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.NOT_FOUND);
+	}	
+		
 }

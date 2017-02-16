609e5307721cd5e3f03561a045a090254e12694a
nontster <nontster@192.168.1.112>
2017-02-16 14:03:46 +0700

    Update security configuration

--- src/main/java/th/co/ais/enterprisecloud/security/SecurityConfiguration.java
+++ src/main/java/th/co/ais/enterprisecloud/security/SecurityConfiguration.java
@@ -1,7 +1,6 @@
 package th.co.ais.enterprisecloud.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -14,8 +13,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-	private static String REALM="LAZYBEE_REALM";
 	
 	@Autowired
 	public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
@@ -28,14 +25,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         .authorizeRequests()
         .antMatchers("/api/**").hasRole("USER")
         .antMatchers("/api/admin/**").hasRole("ADMIN")
-        .and().httpBasic().realmName(REALM).authenticationEntryPoint(getBasicAuthEntryPoint())
+        .and().httpBasic()
         .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);//We don't need sessions to be created.
 	}
-
-	@Bean
-    public CustomBasicAuthenticationEntryPoint getBasicAuthEntryPoint(){
-        return new CustomBasicAuthenticationEntryPoint();
-    }
      
     /* To allow Pre-flight [OPTIONS] request from browser */
     @Override
